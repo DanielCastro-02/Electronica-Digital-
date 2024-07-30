8437167d4d565880d283fab3c38b0a1ccc6b3a97
Santiago Valderrama Monroy <69223535+ASHVALDE@users.noreply.github.com>
2024-07-29 21:30:59 -0500

    Sumador De 3 Bits BCD 2 Digitos

--- lab3/Segmentos/Segmentos.v
+++ lab3/Segmentos/Segmentos.v
@@ -1,12 +1,17 @@
-module Segmentos (A,B,Ci, SSeg, an);
+module Segmentos (MHZ,A,B,Ci, SSeg, an);
 
+
+	localparam divisor = 50000000;
+	reg [25:0] counter = 0;
+	reg clock = 0;
+	input MHZ;
   input [2:0]A;
   input [2:0]B;
   input Ci;
   
   wire [3:0] S;
   wire Co;
-  reg clock = 0;
+  
 	wire C1;
 	 wire C2;
 
@@ -18,21 +23,27 @@ module Segmentos (A,B,Ci, SSeg, an);
  
   output reg [0:6] SSeg;
   output reg [3:0] an;
-  
-  
 
-always @ ( * ) begin
-	if(clock==0)begin
-		clock = 1;
-		an = 4'b1110;
-		
+  
+always@(*) begin
+if (counter < divisor) 
+	begin
+		counter <= counter + 1;
 	end
-	else begin
-		clock = 0;
-		an = 4'b1101;
+else
+	begin
+		counter <= 0;
+		clock <= ~clock; 
 	end
-	
-  case (S)
+end
+
+
+
+
+always@(clock) begin
+	if(clock==1) begin
+		an <= 4'b1110;
+		  case (S)
 
                     //abcdegf      
     4'b0000: SSeg = 7'b0000001; // "0"  
@@ -45,15 +56,43 @@ always @ ( * ) begin
 	4'b0111: SSeg = 7'b0001111; // "7" 
 	4'b1000: SSeg = 7'b0000000; // "8"  
 	4'b1001: SSeg = 7'b0000100; // "9" 
-  4'ha: SSeg = 7'b0001000;  
-  4'hb: SSeg = 7'b1100000;
-  4'hc: SSeg = 7'b0110001;
-  4'hd: SSeg = 7'b1000010;
-  4'he: SSeg = 7'b0110000;
-  4'hf: SSeg = 7'b0111000;
+  4'ha: SSeg = 7'b0000001;  
+  4'hb: SSeg = 7'b1001111;
+  4'hc: SSeg = 7'b0010010;
+  4'hd: SSeg = 7'b0000110;
+  4'he: SSeg = 7'b1001100;
+  4'hf: SSeg = 7'b0100100;
     default:
-    SSeg = 0;
+    SSeg <= 0;
   endcase
-  
+		end
+	else begin
+		an <= 4'b1101;
+		  case (S)
+
+                    //abcdegf      
+    4'b0000: SSeg = 7'b0000001; // "0"  
+	 4'b0001: SSeg = 7'b0000001; // "1" 
+	4'b0010: SSeg = 7'b0000001; // "2" 
+	4'b0011: SSeg = 7'b0000001; // "3" 
+	4'b0100: SSeg = 7'b0000001; // "4" 
+	4'b0101: SSeg = 7'b0000001; // "5" 
+	4'b0110: SSeg = 7'b0000001; // "6" 
+	4'b0111: SSeg = 7'b0000001; // "7" 
+	4'b1000: SSeg = 7'b0000001; // "8"  
+	4'b1001: SSeg = 7'b0000001; // "9" 
+  4'ha: SSeg = 7'b1001111;  
+  4'hb: SSeg = 7'b1001111;
+  4'hc: SSeg = 7'b1001111;
+  4'hd: SSeg = 7'b1001111;
+  4'he: SSeg = 7'b1001111;
+  4'hf: SSeg = 7'b1001111;
+    default:
+    SSeg <= 0;
+  endcase
+	end
+	
 end
+
+
 endmodule
--- lab3/Segmentos/output_files/Segmentos.cdf
+++ lab3/Segmentos/output_files/Segmentos.cdf
@@ -4,7 +4,7 @@ JedecChain;
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(EP4CE10E22) Path("/home/daniel/Segmentos/output_files/") File("Segmentos.sof") MfrSpec(OpMask(1));
+		Device PartName(EP4CE10E22) Path("C:/Users/valde/Documents/GitHub/Electronica-Digital-G2-E1/lab3/Segmentos/output_files/") File("Segmentos.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
